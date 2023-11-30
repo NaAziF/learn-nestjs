ede5d39fcc7c9cc5e0e92f1ee2bdc521cc991756
NaZiF dar <faizan.dar@balancenutrition.in>
2023-11-30 17:39:50 +0530

    added Double Dependency in Auth Controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,9 +1,13 @@
 import { Controller, Post, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { MongooseService } from '../mongoose.db/mongoose.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private AuthService: AuthService) {}
+  constructor(
+    private AuthService: AuthService,
+    private MongooseService: MongooseService,
+  ) {}
   @Post()
   logIn(@Req() req: any) {
     return this.AuthService.checkLogIn(req.body.userName, req.body.password);
