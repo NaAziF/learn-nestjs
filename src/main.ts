0df257fd81c130b8a533b31d6f4de02b96a5c88a
NaZiF dar <faizan.dar@balancenutrition.in>
2023-12-01 13:03:34 +0530

    added basic web socket.io gateway functionlity

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: ['*'],
+      methods: ['*'],
+      credentials: true,
+    },
+  });
+
   await app.listen(3000);
 }
 bootstrap();
--- src/modules/chats/chat.gateway.ts
+++ src/modules/chats/chat.gateway.ts
@@ -6,7 +6,13 @@ import {
 } from '@nestjs/websockets';
 import { ChatService } from './chat.service';
 
-@WebSocketGateway()
+@WebSocketGateway({
+  cors: {
+    origin: ['*'],
+    methods: ['*'],
+    credentials: true,
+  },
+})
 export class ChatGateway {
   constructor(private ChatService: ChatService) {}
 
@@ -14,7 +20,8 @@ export class ChatGateway {
   server;
   @SubscribeMessage('message')
   handleMessage(@MessageBody() message: string): void {
-    this.server.emit('message', message);
+    this.server.emit('message', ' hello Your data recived');
+    console.log(message);
     this.ChatService.saveMessage(message);
   }
 }
