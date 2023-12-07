67c3cce844db901bbca353f560f4863520d5836a
NaZiF dar <faizan.dar@balancenutrition.in>
2023-12-07 12:44:15 +0530

    added auth guard and implemented guard on all chat endpoints

--- src/modules/chats/chat.controller.ts
+++ src/modules/chats/chat.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Patch, Post, Delete } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Patch,
+  Post,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { ChatService } from './chat.service';
 import {
   SendMessageDto,
@@ -6,26 +14,27 @@ import {
   UpdateMessageDto,
   DeleteMessageDto,
 } from './dto';
+import { AuthGuard } from '../auth/guard/auth.guard';
 
 @Controller('chat')
 export class ChatController {
   constructor(private ChatService: ChatService) {}
-
+  @UseGuards(AuthGuard)
   @Post()
   async sendMessage(@Body() req: SendMessageDto): Promise<object> {
     return await this.ChatService.saveMessage(req);
   }
-
+  @UseGuards(AuthGuard)
   @Get()
   async getMessage(@Body() req: RecieveMessageDto): Promise<object> {
     return await this.ChatService.getMessage(req);
   }
-
+  @UseGuards(AuthGuard)
   @Patch()
   async updateMessage(@Body() req: UpdateMessageDto): Promise<object> {
     return await this.ChatService.updateMessage(req);
   }
-
+  @UseGuards(AuthGuard)
   @Delete()
   async deleteMessage(@Body() req: DeleteMessageDto): Promise<object> {
     return await this.ChatService.deleteMessage(req.MessageId);
