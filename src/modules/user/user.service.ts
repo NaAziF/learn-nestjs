19a9713edac097f1421bf74967ff191162cafced
NaZiF dar <faizan.dar@balancenutrition.in>
2023-12-08 11:34:42 +0530

    demo changes

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class UserService {
   sayHello(): string {
-    return 'hello FROM user service';
+    return 'hello from user service this is public endpoint';
   }
 }
